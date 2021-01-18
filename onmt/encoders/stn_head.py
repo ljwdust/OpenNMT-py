fixed bugs: model.eval(), normalization

onmt/encoders/stn_head.py
@@ -48,10 +48,24 @@ class STNHead(nn.Module):
     )
     self.stn_fc2 = nn.Linear(1024, num_ctrlpoints*2)
 
+    padW = 24
+    padH = 6
+    h = 36
+    w = 144
+    target_control_points = torch.Tensor(np.float32(
+                        [ [padW, padH], 
+                          [padW, h-1+padH], 
+                          [w-1+padW, h-1+padH], 
+                          [w-1+padW, padH] 
+                        ]))
 
-    self.init_weights(self.stn_convnet)
-    self.init_weights(self.stn_fc1)
-    self.init_stn(self.stn_fc2)
+    bias = target_control_points.view(-1)
+    self.stn_fc2.bias.data.copy_(bias)
+    self.stn_fc2.weight.data.zero_()
+
+    # self.init_weights(self.stn_convnet)
+    # self.init_weights(self.stn_fc1)
+    # self.init_stn(self.stn_fc2)
 
 
   def init_weights(self, module):
@@ -86,22 +100,15 @@ class STNHead(nn.Module):
 
   def forward(self, x):
     x = self.stn_convnet(x)
-    batch_size, _, h, w = x.size()
-    x = x.view(batch_size, -1)
-    img_feat = self.stn_fc1(x)
-    x = self.stn_fc2(0.1 * img_feat)
-    if self.activation == 'sigmoid':
-      x = F.sigmoid(x)
+    x = x.view(-1, self.numel)
+    x = self.stn_fc2(self.stn_fc1(x))
     x = x.view(-1, self.num_ctrlpoints, 2)
-    return img_feat, x
-
-
-if __name__ == "__main__":
-  in_planes = 3
-  num_ctrlpoints = 20
-  activation='none' # 'sigmoid'
-  stn_head = STNHead(in_planes, num_ctrlpoints, activation)
-  input = torch.randn(10, 3, 32, 64)
-  control_points = stn_head(input)    
-  print(control_points.size())
-  
+    # x = self.stn_convnet(x)
+    # batch_size, _, h, w = x.size()
+    # x = x.view(batch_size, -1)
+    # img_feat = self.stn_fc1(x)
+    # x = self.stn_fc2(img_feat)
+    # if self.activation == 'sigmoid':
+    #   x = F.sigmoid(x)
+    # x = x.view(-1, self.num_ctrlpoints, 2)
+    return x, x

onmt/models/model.py
@@ -53,13 +53,17 @@ class NMTModel(nn.Module):
 
         if self.rectifier is not None:
             # input images are downsampled before being fed into stn_head.
-            stn_input = F.interpolate(src, self.tps_inputsize, mode='bilinear', align_corners=True)
-            stn_img_feat, ctrl_points = self.model_stn_head(stn_input)
+            stn_input = kornia.geometry.transform.resize(src, self.tps_inputsize, interpolation='bicubic')
+            stn_input = stn_input.clamp(0,1)
+            stn_input = (stn_input - 0.5) / 0.5
+            _, ctrl_points = self.model_stn_head(stn_input)
 
-            self.cnt += 1
-            skimage.io.imsave(str(self.cnt)+'-1.jpg', src[0].squeeze().cpu().numpy())
+            # self.cnt += 1
+            # skimage.io.imsave(str(self.cnt)+'-1.jpg', src[0].squeeze().cpu().numpy())
+            
             # src, _ = self.model_tps(src, ctrl_points)
 
+            ############### using kornia ##################
             dst_h, dst_w = src.shape[2:4]
             points_dst = torch.tensor([[
                 [0., 0.], [dst_w - 1., 0.], [dst_w - 1., dst_h - 1.], [0., dst_h - 1.],
@@ -67,19 +71,16 @@ class NMTModel(nn.Module):
             points_dst = points_dst.repeat(src.shape[0], 1, 1)
             points_dst = points_dst.cuda()
 
-            # p_dst = torch.cat((points_dst, points_dst), 0)
-            # p_dst = p_dst.cuda()
-
             ctrl_points2 = ctrl_points.clone()
-            ctrl_points[:,2,:] = ctrl_points2[:,3,:]
-            ctrl_points[:,3,:] = ctrl_points2[:,2,:]
-            ctrl_points[:,:,0] *= dst_w
-            ctrl_points[:,:,1] *= dst_h
+            ctrl_points[:,1,:] = ctrl_points2[:,3,:]
+            ctrl_points[:,3,:] = ctrl_points2[:,1,:]
+            ctrl_points[:,:,0] = ctrl_points[:,:,0] / 192 * dst_w
+            ctrl_points[:,:,1] = ctrl_points[:,:,1] / 48 * dst_h
 
             M = kornia.get_perspective_transform(ctrl_points, points_dst)
             src = kornia.warp_perspective(src, M, dsize=(dst_h, dst_w), border_mode='border')
-
-            skimage.io.imsave(str(self.cnt)+'-2.jpg', src[0].squeeze().cpu().detach().numpy())
+            
+            # skimage.io.imsave(str(self.cnt)+'-kornia.jpg', src[0].squeeze().cpu().detach().numpy())
 
         enc_state, memory_bank, lengths = self.encoder(src, lengths)
 